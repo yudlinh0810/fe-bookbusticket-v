92fed16944dd636a5b28e601cf2e4de69323c8cb
yudlinh0810 <yudlinh0810@gmail.com>
2024-12-01 18:50:33 +0700

    feat: integrate react-toastify for global toast notifications

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
-import TestPage from './pages/TestPage';
+import Home from './pages/Home/HomePage';
+import Login from './components/Information/Login/Login';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
-    <Routes>
-      <Route exact path='/' element={<TestPage />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} /> <Route path='/login' element={<Login />} />
+      </Routes>
+      <ToastContainer
+        position='top-right'
+        autoClose={1000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme='colored'
+      />
+    </Router>
   );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,16 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const queryClient = new QueryClient();
 
 root.render(
   <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </QueryClientProvider>
 );
 
