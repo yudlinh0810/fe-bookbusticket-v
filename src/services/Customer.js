7b23a6f9b0cf63157666eca31c05f6f5826035c3
yudlinh0810 <yudlinh0810@gmail.com>
2024-12-01 22:52:58 +0700

    remove CORS

--- src/services/Customer.js
+++ src/services/Customer.js
@@ -1,21 +1,15 @@
 import bookBusTicketAPI from './CustomizeAxios';
 
 const login = (customer) => {
-  return bookBusTicketAPI.post(`/customer/login`, customer, {
-    headers: { 'Content-Type': 'application/json' },
-  });
+  return bookBusTicketAPI.post(`/customer/login`, customer);
 };
 
 const register = (newCustomer) => {
-  return bookBusTicketAPI.post(`/customer/register`, newCustomer, {
-    headers: { 'Content-Type': 'application/json' },
-  });
+  return bookBusTicketAPI.post(`/customer/register`, newCustomer);
 };
 
 const verifyEmail = (verify) => {
-  return bookBusTicketAPI.post(`/customer/verify-email`, verify, {
-    headers: { 'Content-Type': 'application/json' },
-  });
+  return bookBusTicketAPI.post(`/customer/verify-email`, verify);
 };
 
 export { login, register, verifyEmail };
