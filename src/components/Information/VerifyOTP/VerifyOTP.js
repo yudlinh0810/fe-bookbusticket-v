6bca2cc002fd4b509e97d1bc4177cef5905cb7f9
yudlinh0810 <yudlinh0810@gmail.com>
2024-12-03 22:24:20 +0700

    update: VerifyOTP implement API call for user details

--- src/components/Information/VerifyOTP/VerifyOTP.js
+++ src/components/Information/VerifyOTP/VerifyOTP.js
@@ -3,19 +3,37 @@ import './verify_otp.scss';
 import Header from '../../Header/Header';
 import { useLocation, useNavigate } from 'react-router-dom';
 import OTPInput from '../../OTPInput/OTPInput';
-import { verifyEmail } from '../../../services/Customer';
+import { fetchCustomer, verifyEmail } from '../../../services/Customer';
 import { toast } from 'react-toastify';
+import useUserStore from '../../../stores/UserStore';
 
 const VerifyOTP = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const email = location.state.email || null;
+  const { user, setUser } = useUserStore();
+  console.log('user', user);
+
   const handleOTPVerify = async (data) => {
     const res = await verifyEmail(data);
     console.log('res', res);
     if (res[0].status !== 'OK') {
       toast.error('Bạn đã nhập sai mã OTP');
     } else {
+      const detail = await fetchCustomer(res[0]);
+      console.log('detail', detail);
+      const result = {
+        id: detail.id,
+        email: detail.email,
+        name: detail.name,
+        phone: detail.phone,
+        portrait: detail.portrait,
+        role_id: detail.role_id,
+        status_id: detail.status_id,
+        address: detail.address,
+        day_birth: detail.day_birth,
+      };
+      setUser(result);
       toast.success('Đăng ký tài khoản thành công');
       navigate('/', {
         state: { email: data.email, name: data.name },
