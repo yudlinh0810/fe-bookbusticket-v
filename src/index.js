391bcde12d7727bc82983d3b573aa9dd973f926d
yudlinh0810 <yudlinh0810@gmail.com>
2024-12-19 12:52:26 +0700

    feat: add Slider for home page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -6,9 +6,18 @@ import Slide from '../../components/Slide/Slide';
 import slide_img from '../../assets/images/slide-img.jpg';
 import slide2_img from '../../assets/images/slide2-img.jpg';
 import slide3_img from '../../assets/images/slide3-img.jpg';
+import Slider from 'react-slick';
+import './home.scss';
 
 const HomePage = () => {
   const navigate = useNavigate();
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+  };
   const slideArr = [
     {
       title: `LET'S EXPLORE THE VIETNAM`,
@@ -39,6 +48,144 @@ const HomePage = () => {
       </div>
       <div style={{ display: 'flex', justifyContent: 'center' }}>
         <SearchTripCpn onSubmit={handleSearchTrip} />
+      </div>
+      <div className='list-content'>
+        <div className='route'>
+          <h4>Tuyến đường phổ biến</h4>
+          <Slider {...settings}>
+            <div className='slider-item'>
+              <img src='https://f1e425bd6cd9ac6.cmccloud.com.vn/cms-tool/post/images/219/img_card.png?v=2' />
+              <div className='description'>
+                <span>Đà Nẵng - Nha Trang</span>
+                <span>Giá: 250.000</span>
+              </div>
+            </div>
+            <div className='slider-item'>
+              <img src='https://f1e425bd6cd9ac6.cmccloud.com.vn/cms-tool/destination/images/24/img_hero.png' />
+              <div className='description'>
+                <span>Đà Nẵng - Đà Lạt</span>
+                <span>Giá: 250.000</span>
+              </div>
+            </div>
+            <div className='slider-item'>
+              <img src='https://f1e425bd6cd9ac6.cmccloud.com.vn/cms-tool/destination/images/24/img_hero.png' />
+              <div className='description'>
+                <span>Đà Nẵng - Hà Nội</span>
+                <span>Giá: 250.000</span>
+              </div>
+            </div>
+            <div className='slider-item'>
+              <img src='https://f1e425bd6cd9ac6.cmccloud.com.vn/cms-tool/destination/images/22/img_hero.png' />
+              <div className='description'>
+                <span>Đà Nẵng - Sài gòn</span>
+                <span>Giá: 250.000</span>
+              </div>
+            </div>
+            <div className='slider-item'>
+              <img src='https://f1e425bd6cd9ac6.cmccloud.com.vn/cms-tool/destination/images/5/img_hero.png?v1' />
+              <div className='description'>
+                <span>Đà Nẵng - Sapa</span>
+                <span>Giá: 250.000</span>
+              </div>
+            </div>
+            <div className='slider-item'>
+              <img src='https://f1e425bd6cd9ac6.cmccloud.com.vn/cms-tool/destination/images/5/img_hero.png?v1' />
+              <div className='description'>
+                <span>Đà Nẵng - Gia Lai</span>
+                <span>Giá: 250.000</span>
+              </div>
+            </div>
+          </Slider>
+        </div>
+        <div className='new'>
+          <h4>Vexe có gì mới</h4>
+          <Slider {...settings}>
+            <div className='slider-item'>
+              <img src='https://f1e425bd6cd9ac6.cmccloud.com.vn/cms-tool/post/images/219/img_card.png?v=4' />
+              <div className='description'>
+                <span>Tận hưởng nhiều ưu đãi và các tính năng mới cùng Siêu ứng dụng Vexere</span>
+              </div>
+            </div>
+            <div className='slider-item'>
+              <img src='https://f1e425bd6cd9ac6.cmccloud.com.vn/cms-tool/post/images/217/img_card.png?v=2' />
+              <div className='description'>
+                <span>Các tính năng mới cùng Siêu ứng dụng Vexere</span>
+              </div>
+            </div>
+            <div className='slider-item'>
+              <img src='https://f1e425bd6cd9ac6.cmccloud.com.vn/cms-tool/post/images/200/img_card.png?v=12' />
+              <div className='description'>
+                <span>“Bảo hiểm chuyến đi” chính thức ra mắt tại Vexere</span>
+              </div>
+            </div>
+            <div className='slider-item'>
+              <img src='https://f1e425bd6cd9ac6.cmccloud.com.vn/cms-tool/post/images/212/img_card.png?v=14' />
+              <div className='description'>
+                <span>Chương trình tích điểm đổi quà tại Vexere</span>
+              </div>
+            </div>
+            <div className='slider-item'>
+              <img src='https://f1e425bd6cd9ac6.cmccloud.com.vn/cms-tool/post/images/219/img_card.png?v=13' />
+              <div className='description'>
+                <span>Tận hưởng nhiều ưu đãi và các tính năng mới cùng Siêu ứng dụng Vexere</span>
+              </div>
+            </div>
+            <div className='slider-item'>
+              <img src='https://f1e425bd6cd9ac6.cmccloud.com.vn/cms-tool/post/images/219/img_card.png?v=2' />
+              <div className='description'>
+                <span>Tận hưởng nhiều ưu đãi và các tính năng mới cùng Siêu ứng dụng Vexere</span>
+              </div>
+            </div>
+          </Slider>
+        </div>
+        <div className='media'>
+          <h4>Vexe đã được nhắc đến trên</h4>
+          <div className='media-list'>
+            <div className='row'>
+              <div className='item col'>
+                <img src='https://229a2c9fe669f7b.cmccloud.com.vn/images/logo-baochi/logo-vne.png' />
+              </div>
+              <div className='item col'>
+                <img src='https://229a2c9fe669f7b.cmccloud.com.vn/images/logo-baochi/logo-vtv.png' />
+              </div>
+              <div className='item col'>
+                <img src='https://229a2c9fe669f7b.cmccloud.com.vn/images/logo-baochi/logo-cesti.png' />
+              </div>
+              <div className='item col'>
+                <img src='https://229a2c9fe669f7b.cmccloud.com.vn/images/logo-baochi/logo-dantri.png' />
+              </div>
+              <div className='item col'>
+                <img src='	https://229a2c9fe669f7b.cmccloud.com.vn/images/logo-baochi/logo-tuoitre.png' />
+              </div>
+              <div className='item col'>
+                <img src='https://229a2c9fe669f7b.cmccloud.com.vn/images/logo-baochi/logo-fbnc.png' />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='carry'>
+          <h4>Bến xe khách</h4>
+          <div className='carry-list'>
+            <div className='row'>
+              <div className='item col'>
+                <img src='https://cdn1.nhatrangtoday.vn/images/photos/xe-phuong-trang-top.jpg' />
+                <span>Bến xe Phương Trang</span>
+              </div>
+              <div className='item col'>
+                <img src='https://xevati.com/wp-content/uploads/2021/10/Xe-khach-dau-trong-ben-xe-da-nang.jpg' />
+                <span>Bến xe Miền Đông</span>
+              </div>
+              <div className='item col'>
+                <img src='https://xevati.com/wp-content/uploads/2021/10/Xe-khach-dau-trong-ben-xe-da-nang.jpg' />
+                <span>Bến xe Miền Đông</span>
+              </div>
+              <div className='item col'>
+                <img src='https://xevati.com/wp-content/uploads/2021/10/Xe-khach-dau-trong-ben-xe-da-nang.jpg' />
+                <span>Bến xe Miền Đông</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
