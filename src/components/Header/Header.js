cdd277c4714ad1e2d66bfc846f5440a838d3d053
yudlinh0810 <yudlinh0810@gmail.com>
2025-01-12 13:18:10 +0700

    update: scss reposive for mobile

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,24 +11,25 @@ const Header = () => {
     clearUser();
   };
   return (
-    <div className='header-container'>
-      <div className='header-left'>
-        <Link to={'/'}>
+    <header className='header__container'>
+      <div className='header__left'>
+        <Link to={'/'} className='header__logo'>
           <img src={logo} alt='logo' width={200} height={200} />
         </Link>
-        <Link className='h-l-content' href='/'>
+        <Link className='header__left__content' href='/'>
+          <p className='title'>Vexetienich </p>
           Cam kết hoàn 150% nếu nhà xe không cung cấp dịch vụ vận chuyển
         </Link>
-        <div className='h-l-icon'>
+        <div className='header__left__icon'>
           <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512' height={15} width={15}>
             <path d='M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-384c13.3 0 24 10.7 24 24l0 112c0 13.3-10.7 24-24 24s-24-10.7-24-24l0-112c0-13.3 10.7-24 24-24zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z' />
           </svg>
         </div>
       </div>
-      <ul className='header-right'>
-        <li className='hd-r-li-w '>Đơn hàng của tôi</li>
-        <li className='hd-r-li-w '>Mở bán vé trên Vexetienich</li>
-        <li className='hd-r-li-w '>Trở thành đối tác</li>
+      <ul className='event-list'>
+        <li className='event-item '>Đơn hàng của tôi</li>
+        <li className='event-item '>Mở bán vé trên Vexetienich</li>
+        <li className='event-item '>Trở thành đối tác</li>
         <li className='hd-r-li'>
           <Link to={'/'}>
             <img
@@ -40,7 +41,7 @@ const Header = () => {
           </Link>
         </li>
         {location.pathname === '/login-register' ? null : (
-          <li className='hd-r-li'>
+          <li className='event-item'>
             {user ? (
               <div className='btn-hd-r-li'>
                 <div className='username'>{user?.name}</div>
@@ -82,7 +83,7 @@ const Header = () => {
             )}
           </li>
         )}
-        <li className='hd-r-li-w '>
+        <li className='event-item '>
           <div className='hd-r-support'>
             <div className='h-d-icon'>
               <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512' height={16} width={16}>
@@ -93,7 +94,7 @@ const Header = () => {
           </div>
         </li>
       </ul>
-    </div>
+    </header>
   );
 };
 
